45f41261ee7b958e66106b7d188a173d4cfa18dd
Biancadesck <bianca.desck@escola.pr.gov.br>
2026-08-12 18:43:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ const vestibulares = {
     },
     unicentro: { 
         name: "UNICENTRO", 
-        date: "2026-09-21T14:00:00", 
+        date: "2026-10-11T14:00:00", 
         formatted: "21/09/2026",
         questoes: "40 questões objetivas + Redação",
         redacao: "Dissertativo-argumentativa",
